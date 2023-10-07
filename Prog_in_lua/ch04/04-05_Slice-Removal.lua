671e6d65c21c2aed76bc88f7df72fc98863e210f
Xiaoqi Zhao <xiaoqi.zhao@volvo.com>
2023-10-07 14:53:10 -0400

    Chapter 4 exercise

--- Prog_in_lua/ch04/04-05_Slice-Removal.lua
+++ Prog_in_lua/ch04/04-05_Slice-Removal.lua
@@ -1,2 +1,15 @@
 -- Write a function to remove a slice from a string
 -- the slice should be given by its initial position and its length
+
+function remove(s, p, l)
+
+    if p+l > string.len(s) then
+        l = string.len(s) - p + 1
+    end
+
+    s_left = string.sub(s, 1, p-1)
+    s_right = string.sub(s, p+l, -1)
+    print(s_left .. s_right)
+end
+
+remove("hello world", 7, 3)
--- Prog_in_lua/ch04/04-07_palindrome.lua
+++ Prog_in_lua/ch04/04-07_palindrome.lua
@@ -1,1 +1,16 @@
 -- Write a function to check whether a given string is a palindrome
+
+function ispali(s)
+    -- print(s)
+    s_rev = string.reverse(s)
+    -- print(s_rev)
+    if s == s_rev then
+        print("It's Palindrome")
+    else
+        print("It's not Palindrome")
+    end
+end
+
+ispali("step on no pets")
+
+ispali("banana")
